75a21eb8f1795183c17ff19cb9d59c57f03ff174
Daniel Sbeiti <sb.danielh@gmail.com>
2025-03-05 02:27:56 -0500

    update settings

--- TalkTuahDan/settings.py
+++ TalkTuahDan/settings.py
@@ -126,7 +126,7 @@ STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Add these at the end of the file
 MEDIA_URL = '/media/'
