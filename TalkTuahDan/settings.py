912ea316be658e5fc03e5d4ee7a7b69a3f840053
Daniel Sbeiti <sb.danielh@gmail.com>
2025-03-06 16:07:11 -0500

    domain

--- TalkTuahDan/settings.py
+++ TalkTuahDan/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['13.61.105.202', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['16.171.39.175', 'localhost', '127.0.0.1', "danielsbeiticv.com", "www.danielsbeiticv.com"]
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 
